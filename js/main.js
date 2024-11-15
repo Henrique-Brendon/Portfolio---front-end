047b7adbd4bcf77a1f4bfc612fe318176722d443
Henrique Brendon <henriquebrendonti26@gmail.com>
2024-11-15 15:42:30 -0300

    Portfolio Filter completion - desktop

--- js/main.js
+++ js/main.js
@@ -20,3 +20,32 @@ const tabs = document.querySelectorAll('[data-target]'),
             
         })
     })
+
+/*========= Filter job cards =========*/
+document.addEventListener("DOMContentLoaded", () => {
+    const filterItems = document.querySelectorAll(".work__item"); // Filter Buttons
+    const workCards = document.querySelectorAll(".work__card");  // Job cards
+
+    filterItems.forEach(item => {
+        item.addEventListener("click", () => {
+            // Remove the 'active-work' class from all buttons
+            filterItems.forEach(filter => filter.classList.remove("active-work"));
+
+            // Adds the 'active-work' class to the clicked button
+            item.classList.add("active-work");
+
+            const filter = item.dataset.filter; // Gets the data-filter value of the clicked button
+
+            // Show or hide cards based on filter
+            workCards.forEach(card => {
+                if (filter === "all") {
+                    card.classList.remove("hide"); // Show all cards
+                } else if (card.classList.contains(filter.substring(1))) {
+                    card.classList.remove("hide"); // Shows matching cards
+                } else {
+                    card.classList.add("hide");; // Hide unmatched cards
+                }
+            });
+        });
+    });
+});
