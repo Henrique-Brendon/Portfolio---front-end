4d70d4e2ae9ac5475efb6d8e749426ed6a86def7
Henrique Brendon <henriquebrendonti26@gmail.com>
2024-11-18 17:23:05 -0300

    Breakingpoint - small device

--- js/main.js
+++ js/main.js
@@ -146,3 +146,21 @@ function navHighlighter() {
         }
     });
 }
+
+
+/*=================== SHOW SIDEBAR ====================*/
+const navMenu = document.getElementById('sidebar'),
+    navToggle =  document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
+
+if(navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add('show-sidebar');
+    });
+}
+
+if(navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove('show-sidebar');
+    });
+}
